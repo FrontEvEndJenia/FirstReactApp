44c869a0e1c6ec6b151901ff377efc78fe8cd3bf
Evgeniy Sych <evgenius007007@gmail.com>
2025-05-07 20:04:58 +0300

    добавление проекта vite и настроек

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ReactLogo from './assets/react.svg'
+import './index.css'
+
+export const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={ReactLogo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> Реактивно работает реакт.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
+}
